c225d781d1d9bc0e6c22739bd8e36d66d14c2db9
KDragonM <kennethmitra@gmail.com>
2016-11-29 18:40:24 -0600

    Added Bootstrap

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 <html>
 	<head>
 		<title>PointsPage</title>
+		<!-- Bootstrap - Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+
 	</head>
 	<body>
 		<h1>TEST OK</h1>
+		
+		
+		
+		
+		
+		
+		
+        <!-- Bootstrap - Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</body>
 </html>
 
