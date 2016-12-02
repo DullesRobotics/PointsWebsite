ca959c45ebfe0e1e8b97ed49031cb3ce554d8dae
KDragonM <kennethmitra@gmail.com>
2016-12-01 22:39:30 -0600

    Added connection to MYSQL DB

--- reply.php
+++ reply.php
@@ -12,12 +12,23 @@
             $id = trim(str_replace("}", "", $contents[1]));
             echo "      ID: " . $id;
             
-            echo $USERNAME;
             
             $file = fopen("logs/scannedID.txt","a+") or die("cant open/create file");
             fwrite($file,$id."\n");
             fclose($file);
             
+
+
+        try {
+            $conn = new PDO("mysql:host=$SERVERNAME;dbname=$DBNAME", $USERNAME, $PASSWORD);
+         // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+         echo "Connected successfully"; 
+          }
+        catch(PDOException $e)
+            {
+                echo "Connection failed: " . $e->getMessage();
+            }
             
         ?>
         <h3>Cron job test</h3>
