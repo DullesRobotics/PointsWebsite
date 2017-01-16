ac94da4b0ca2e8aea0786d41210da80f59afd570
freezone2000 <dev576829@gmail.com>
2017-01-15 20:12:19 -0600

    grsgdr

--- index.php
+++ index.php
@@ -2,30 +2,22 @@
 <html>
 	<head>
 		<title>PointsPage</title>
-		<!-- Bootstrap - Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+			<!-- Bootstrap - Latest compiled and minified CSS -->
+        		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <!-- jQuery library -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-
 	</head>
 	<body>
-		<nav class="navbar navbar-inverse"
-		<div class="jumbotron">
-		    <h1>Dulles Robotics Club Points Page</h1>
-		</div>
-		<?php
-		//echo phpinfo();
-		?>
-		
-		
-		
-		
-		
-		
-		
-        <!-- Bootstrap - Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<nav class="navbar navbar-inverse"
+	<div class="jumbotron" style="text-align:center;">
+		<h1>
+		<font color="red">
+	    		Dulles Robotics Club Points Page
+		</font>
+		</h1>
+	</div>
+        		<!-- Bootstrap - Latest compiled JavaScript -->
+       	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</body>
 </html>
 
