76adb37ea924f1561146669c7afd17461a424abc
freezone2000 <dev576829@gmail.com>
2017-01-15 19:11:18 -0600

    stoof

--- reply.php
+++ reply.php
@@ -10,8 +10,8 @@
             echo "Raw: " . $raw;
             $contents = split(":", $raw);
             //$id = trim(str_replace("}", "", $contents[1]));
-            $id = trim(substr($contents[1],1,-6));
-            echo "      ID: " .($id) ? 'No Value';
+            $id = trim(substr($contents[1],1,-6)) ? 'No Value';
+            echo "      ID: " .$id;
             
             
             
