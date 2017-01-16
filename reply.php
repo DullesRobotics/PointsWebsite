76b84aaa787296774e86285cb3883aee69cd24c0
freezone2000 <dev576829@gmail.com>
2017-01-16 02:37:52 -0600

    Update reply.php

--- reply.php
+++ reply.php
@@ -59,7 +59,7 @@
                         } else {
                             echo $person["First_Name"]." ".$person["Last_Name"]." successfully signed in. \n";
                             fwrite($signedLogs,date('Y-m-d H:i:s')." ".$person["First_Name"]." ".$person["Last_Name"]." successfully signed in. \n");
-                            $pointsToAdd = $difference % 60;
+                            $pointsToAdd = $difference/60;
                             echo "Time Difference: ".gmdate("H:i:s", $difference)."\n";
                             echo "Points to add: ".$pointsToAdd."\n";
                         }
