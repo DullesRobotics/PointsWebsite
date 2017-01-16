b2a67ec9e4f24d3777863000a0b9c7e14dac2236
freezone2000 <dev576829@gmail.com>
2017-01-16 02:53:42 -0600

    Update reply.php

--- reply.php
+++ reply.php
@@ -54,7 +54,7 @@
                         echo "Now: ".$currentTime."\n";
                         $difference = $currentTime - $lastScanTime;
                         if ($person["Signed_In"] % 2 == 0){
-                            if ($difference > 1) {
+                            if ($difference > 43200) {
                                 echo "Difference is greater than 12 hours \n";
                                 fwrite($signedLogs,$person["First_Name"]." ".$person["Last_Name"]." did not sign out for over 12 hours and was not awarded points.\n");
                                 break;
@@ -63,7 +63,10 @@
                             echo $person["First_Name"]." ".$person["Last_Name"]." successfully signed out. \n";
                             fwrite($signedLogs,date('Y-m-d H:i:s')." ".$person["First_Name"]." ".$person["Last_Name"]." successfully signed out. Awarded ".$pointsToAdd." points!\n");
                             echo "Time Difference: ".gmdate("H:i:s", $difference)."\n";
-                            echo "Points to add: ".$pointsToAdd."\n";
+                            if ($pointsToAdd > 0) {
+                                $conn->exec("UPDATE Members SET Points = Points + '$pointsToAdd' WHERE Tag_ID = '$id'");
+                                echo "Points awarded: ".$pointsToAdd."\n";
+                            }
                         } else {
                             echo $person["First_Name"]." ".$person["Last_Name"]." successfully signed in. \n";
                             fwrite($signedLogs,date('Y-m-d H:i:s')." ".$person["First_Name"]." ".$person["Last_Name"]." successfully signed in. \n");
