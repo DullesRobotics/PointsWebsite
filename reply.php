bf19b56f34ae26ce3127f92d20755db6559a0189
freezone2000 <dev576829@gmail.com>
2017-01-28 16:59:33 -0600

    Update reply.php

--- reply.php
+++ reply.php
@@ -91,7 +91,23 @@
                     } else {
                         $word = split("$COMMANDSPASSWORD",$raw);
                         if (sizeof($word) > 1){
-                            echo "\nCOMMAND: ".$word[1];
+                            echo "\nAttempting post\n";
+                            $url = 'http://server.com/path';
+                            $data = array('key1' => 'value1', 'key2' => 'value2');
+
+                            // use key 'http' even if you send the request to https://...
+                            $options = array(
+                                'http' => array(
+                                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                                    'method'  => 'POST',
+                                    'content' => http_build_query($data)
+                                )
+                            );
+                            $context  = stream_context_create($options);
+                            $result = file_get_contents($url, false, $context);
+                            if ($result === FALSE) { /* Handle error */ }
+
+                            var_dump($result);
                         }
                     }
                 }
