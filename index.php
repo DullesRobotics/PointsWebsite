3ea4f19045d7adf4da6b70e8112b9fef4ef84ae6
Karim Karim <dev576829@gmail.com>
2017-03-30 20:16:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 	<div style="text-align:center;">
 		<h1>
 		<font color="red">
-	    		<!--Dulles Robotics Club Points Page
+	    		<!--Dulles Robotics Club Points Page-->
 			<!--SITE UNDERGOING CHANGES; IGNORE ANYTHING WEIRD-->
 			
 			TEMPORARILY OFFLINE; CHECK BACK SOON
