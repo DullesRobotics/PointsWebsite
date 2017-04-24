c8952287a17e8757c54afe18501d5ad3d8159ee0
Karim Karim <dev576829@gmail.com>
2017-04-23 20:46:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 	<head>
 		<title>Points Page</title>
 		     <!--<meta http-equiv="refresh" content = "5" />-->
-			<script> 
-				window.location.href = window.location.href;
-			</script>
-		
 			<!-- Bootstrap - Latest compiled and minified CSS -->
         		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <!-- jQuery library -->
