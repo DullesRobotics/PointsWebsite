320da559a598118c671c4796d2a72a70cd0fda7a
Karim Karim <dev576829@gmail.com>
2017-04-25 17:31:59 -0500

    Update ControlPanel.php

--- ControlPanel.php
+++ ControlPanel.php
@@ -72,14 +72,30 @@
 		  <?php
 			foreach ($data as $person) {
 				echo '<a href = "#">'.$person["First_Name"].' '.$person["Last_Name"].'</a>';
-				echo $person["First_Name"]." loaded!";
 			}
 		  ?>
 	  </div>
 	</div>
 	
     <script language="javascript">
-	    
+	    	function myFunction() {
+		    document.getElementById("myDropdown").classList.toggle("show");
+		}
+
+		// Close the dropdown menu if the user clicks outside of it
+		window.onclick = function(event) {
+		  if (!event.target.matches('.dropbtn')) {
+
+		    var dropdowns = document.getElementsByClassName("dropdown-content");
+		    var i;
+		    for (i = 0; i < dropdowns.length; i++) {
+		      var openDropdown = dropdowns[i];
+		      if (openDropdown.classList.contains('show')) {
+			openDropdown.classList.remove('show');
+		      }
+		    }
+		  }
+		}
     </script>
 </body>
 </html>
