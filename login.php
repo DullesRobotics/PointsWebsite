3a743847fb15c2861de7805728f2ffe453f55bd4
Karim Karim <dev576829@gmail.com>
2017-04-25 11:17:54 -0500

    Update login.php

--- login.php
+++ login.php
@@ -92,7 +92,7 @@
 	//alert(myData + "n\" + splitData);
 	var username = splitData[0];
 	var password = splitData[1];
-	alert("MyData? " + myData + "\nUsername? " + username + "\nPassword? " + password);
+	alert("MyData? " + myData + "\nUsername? " + username + "\nPassword? " + password + "\nSplit length: " + splitData.length);
 	function check(form)
 	{
 		if (tries <= 1) {
