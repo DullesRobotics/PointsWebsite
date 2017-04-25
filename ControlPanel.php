d6a84aa293ebfacb254c4ddf5f16f4b443c8ecef
Karim Karim <dev576829@gmail.com>
2017-04-25 07:47:18 -0500

    Update ControlPanel.php

--- ControlPanel.php
+++ ControlPanel.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+
+  <meta charset="UTF-8">
+
+  <title>ControlPanel</title>
+
+</head>
+
+<body oncontextmenu="return false">
+
+  <div class="body"></div>
+		<div class="grad"></div>
+		<br>
+		<form name="login">
+		
+    </form>
+    <script language="javascript">
+      
+    </script>
+</body>
+</html>
