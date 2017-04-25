83de062eb4856a4b8f9d5e22e603df8e8fcc3338
Karim Karim <dev576829@gmail.com>
2017-04-25 17:35:26 -0500

    Update ControlPanel.php

--- ControlPanel.php
+++ ControlPanel.php
@@ -39,6 +39,15 @@
 	.dropbtn:hover, .dropbtn:focus {
 	    background-color: #3e8e41;
 	}
+	#myInput {
+	    border-box: box-sizing;
+	    background-image: url('searchicon.png');
+	    background-position: 14px 12px;
+	    background-repeat: no-repeat;
+	    font-size: 16px;
+	    padding: 14px 20px 12px 45px;
+	    border: none;
+	}
 	.dropdown {
 	    position: relative;
 	    display: inline-block;
@@ -69,6 +78,7 @@
 	<div class="dropdown">
 	  <button onclick="myFunction()" class="dropbtn">Dropdown</button>
 	  <div id="myDropdown" class="dropdown-content">
+	  <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
 		  <?php
 			foreach ($data as $person) {
 				echo '<a href = "#">'.$person["First_Name"].' '.$person["Last_Name"].'</a>';
@@ -95,6 +105,21 @@
 		      }
 		    }
 		  }
+		}
+		
+		function filterFunction() {
+		    var input, filter, ul, li, a, i;
+		    input = document.getElementById("myInput");
+		    filter = input.value.toUpperCase();
+		    div = document.getElementById("myDropdown");
+		    a = div.getElementsByTagName("a");
+		    for (i = 0; i < a.length; i++) {
+			if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+			    a[i].style.display = "";
+			} else {
+			    a[i].style.display = "none";
+			}
+		    }
 		}
     </script>
 </body>
