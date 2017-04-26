72e1433715017a93a61bcd192425bf80ef8e85a0
Karim Karim <dev576829@gmail.com>
2017-04-26 08:05:11 -0500

    Update phpFunctions.php

--- phpFunctions.php
+++ phpFunctions.php
@@ -18,36 +18,44 @@ th {text-align: left;}
 <body>
 
 <?php
-$q = intval($_GET['q']);
+    require("secretSettings.php");
+    $q = intval($_GET['q']);
+    echo $q;
 
-$con = mysqli_connect('localhost','peter','abc123','my_db');
-if (!$con) {
-    die('Could not connect: ' . mysqli_error($con));
-}
+    try{
+        $con = new PDO("mysql:host=$SERVERNAME;dbname=$DBNAME", $USERNAME, $PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e)
+    {
+        echo "\nConnection aborted: " . $e->getMessage();
+        exit;
+    }
+    $getMembers = $conn->prepare("SELECT * FROM Members");
+    $getMembers->execute();
+    $data = $getMembers->fetchAll();
+    $found = false;
+    /*foreach ($data as $person){
+        if ($person[])
+    }*/
 
-mysqli_select_db($con,"ajax_demo");
-$sql="SELECT * FROM user WHERE id = '".$q."'";
-$result = mysqli_query($con,$sql);
-
-echo "<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-<th>Age</th>
-<th>Hometown</th>
-<th>Job</th>
-</tr>";
-while($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['FirstName'] . "</td>";
-    echo "<td>" . $row['LastName'] . "</td>";
-    echo "<td>" . $row['Age'] . "</td>";
-    echo "<td>" . $row['Hometown'] . "</td>";
-    echo "<td>" . $row['Job'] . "</td>";
-    echo "</tr>";
-}
-echo "</table>";
-mysqli_close($con);
+    /*echo "<table>
+        <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Age</th>
+        <th>Hometown</th>
+        <th>Job</th>
+        </tr>";
+    while($row = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>" . $row['FirstName'] . "</td>";
+        echo "<td>" . $row['LastName'] . "</td>";
+        echo "<td>" . $row['Age'] . "</td>";
+        echo "<td>" . $row['Hometown'] . "</td>";
+        echo "<td>" . $row['Job'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";*/
 ?>
 </body>
 </html>
