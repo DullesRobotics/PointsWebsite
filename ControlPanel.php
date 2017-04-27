4f6c0d8670d179e77573a4927400b572b87338c8
Karim Karim <dev576829@gmail.com>
2017-04-27 07:53:29 -0500

    Update ControlPanel.php

--- ControlPanel.php
+++ ControlPanel.php
@@ -109,7 +109,7 @@
 			    // code for IE6, IE5
 			    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 			}
-			xmlhttp.open("GET","http://dhsrobotics.ddns.net/phpFunctions.php?q="+user,true);
+			xmlhttp.open("GET","http://dhsrobotics.ddns.net/DatabaseControls.php?q="+user,true);
 			xmlhttp.send();
 	    	}
 		
