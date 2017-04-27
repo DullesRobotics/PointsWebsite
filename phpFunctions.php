665341c2ac86f10cee7f979459d0af5658a3ff1a
Karim Karim <dev576829@gmail.com>
2017-04-27 07:47:37 -0500

    Update phpFunctions.php

--- phpFunctions.php
+++ phpFunctions.php
@@ -20,7 +20,8 @@ th {text-align: left;}
 <?php
     require("secretSettings.php");
     $user = $_GET['user'];
-    echo $user;
+    $NameSplit = $user.split(' ');
+    echo "First: ".$NameSplit[0]." Last: ".$NameSplit[1];
     //$NameSplit = $user.split('%20');
     //$firstName = $NameSplit[0].substr(1,strlen($NameSplit[0]));
     //$lastName = $NameSplit[1].substr(1,strlen($NameSplit[1]));
