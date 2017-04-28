5c515bb200e93ac35b135f14824cef6c83c403e6
Karim Karim <dev576829@gmail.com>
2017-04-28 14:36:06 -0500

    Update ControlPanel.php

--- ControlPanel.php
+++ ControlPanel.php
@@ -80,7 +80,7 @@
 		function changeBtnName(to)
 		{
         		document.getElementById("myDropdown").value = to.innerHTML;
-			document.getElementById("CurrentPerson").value = to.innerHTML;
+			document.getElementById("CurrentPerson").innerHTML = to.innerHTML;
 		}
 
 		function dropdownClicked(button)
