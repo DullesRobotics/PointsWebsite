d1604343520fccba565f0cf03fe7553c6a46806b
Karim Karim <dev576829@gmail.com>
2017-04-30 21:31:18 -0500

    Update DatabaseControls.php

--- DatabaseControls.php
+++ DatabaseControls.php
@@ -56,7 +56,21 @@ th {text-align: left;}
 
     function addPoints($person,$pointsToAdd,$conn){
          $tagID = $person["Tag_ID"];
-         $conn->exec("UPDATE Members SET Points = 0 WHERE Tag_ID = '$tagId'");
+         //$conn->exec("UPDATE Members SET Points = 0 WHERE Tag_ID = '$tagId'");
+        $url = 'http://dhsrobotics.ddns.net/reply.php';
+        $data = array("karimiscool ".$tagID." change points by ".$pointsToAdd);
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) { echo "\n ERROR: self-post failed"; }
+        var_dump($result);
         echo "\n<br>added ".$pointsToAdd." pts to ".$tagID;
     }
     
