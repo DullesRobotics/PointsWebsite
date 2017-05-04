2366994d4b88a588010ded4444cbe2943689a5c6
Karim Karim <dev576829@gmail.com>
2017-05-03 20:36:21 -0500

    Update ControlPanel.php

--- ControlPanel.php
+++ ControlPanel.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <?php
  	require("secretSettings.php");
-	//if(isset($_COOKIE["admin"]) && $_COOKIE["admin"]){
-		//echo "Allowed! ".$_COOKIE["admin"];
-	//}else {
-		//echo "Nope! ".$_COOKIE["admin"];
-		//echo "<br>".isset($_COOKIE["admin"]);
+	if(isset($_COOKIE["admin"]) && $_COOKIE["admin"]){
+		echo "Allowed! ".$_COOKIE["admin"];
+		unset($_COOKIE["admin"])
+		setcookie("admin",null,-1);
+	}else {
+		echo "Nope! ".$_COOKIE["admin"];
+		echo "<br>".isset($_COOKIE["admin"]);
 	   	header("location:index.php");
-	   	//die;
-	//}
+	   	die;
+	}
 	try{
 		$conn = new PDO("mysql:host=$SERVERNAME;dbname=$DBNAME", $USERNAME, $PASSWORD);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
