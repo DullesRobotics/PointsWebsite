5fe8c190016843167d369ad4f2ff87efccec2735
KDragonM <kennethmitra@gmail.com>
2017-05-06 18:37:58 -0500

    Learning SQL on sqltest.php

--- sqltest.php
+++ sqltest.php
@@ -3,21 +3,31 @@
     <head>
         <title>SQL Test Page</title>
         <style>
-            #main_title{
+            #main_title,#date{
                 margin-left: auto;
                 margin-right: auto;
                 text-align: center;
             }
+            
         </style>
     </head>
     <body>
         <h1 id="main_title">SQL Test Page</h1>
         <?php
             require 'secretSettings.php';
+            echo "<p id='date'>Today is " . date("d/m/Y") . "</p>";
             try{
                 $connection = new PDO("mysql:host=$SERVERNAME;dbname=$DBNAME",$USERNAME,$PASSWORD);
                 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 echo "<h3>Connected sucessfully</h3>";
+                $getMembers = $connection->prepare("SELECT * FROM Members");
+                $getMembers->execute();
+                $MemberData = $getMembers->fetchAll();
+                
+                foreach ($MemberData as $member) {
+                    echo($member . "<br />");
+                }
+                
             }catch(PDOException $e){
                 echo "<h3>Connection Failed: " . $e->getMessage() . "</h3>";
             }
