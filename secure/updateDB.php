2a955b46f2a2a9ca7be9c331b9719ddc8aad8f43
KDragonM <kennethmitra@gmail.com>
2017-05-06 23:03:25 -0500

    Forgot to require secretSettings.php :)

--- secure/updateDB.php
+++ secure/updateDB.php
@@ -1,11 +1,12 @@
 <?php
-
+    require("secretSettings.php");
     echo $_POST["tag"] . "<br>";
     echo $_POST["points"] . "<br>";
     echo $_POST["submit"] . "<br>";
     
     try{
                 $connection = new PDO("mysql:host=$SERVERNAME;dbname=$DBNAME",$USERNAME,$PASSWORD);
+                
                 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 echo "<h3>Connected sucessfully</h3>";
                 $getMembers = $connection->prepare("SELECT * FROM Members WHERE Tag_ID = " . $_POST['tag']);
