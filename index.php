2e6c51e188e7c5d5b5da46fd9ddac6ad2efa567c
KDragonM <kennethmitra@gmail.com>
2017-05-07 10:03:31 -0500

    Improved hidden button

--- index.php
+++ index.php
@@ -20,7 +20,11 @@
         </noscript>
 	</head>
 	<body>
-	<nav class="navbar navbar-inverse"
+	<nav class="navbar navbar-inverse">
+	    <form name="Secure Area" action="/secure">
+            <input style="color: black; background-color: black" type="submit" value="Secure Area">
+        </form>
+	</nav>
 	<div style="text-align:center;">
 		<h1>
 		<font color="red">
@@ -236,9 +240,7 @@
 					<input type="button" onclick="loadLogin()" value="Login" style="padding: 5px 10px;"/>
 			</div>
 		</form>
-		<form name="Secure Area" action="/secure">
-		      <input style="color: black" type="submit" value="Secure Area">
-		</form>
+
 		<script>
 			function loadLogin()
 			{
