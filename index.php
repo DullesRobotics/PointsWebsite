11b0adb56d4195edf9068331fde795785eb46958
Karim Karim <dev576829@gmail.com>
2017-05-13 17:10:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
 		//border-collapse:collapse;
         ?>
         <style type="text/css">
-	    .tg  .spacer{opacity: 0;}
+	    .tg  .spacer{opacity: .9;}
             .tg  {border-spacing:0;border-color:#aabcfe;margin:0px auto;}
             .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#669;background-color:#e8edff;}
             .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#039;background-color:#b9c9fe;}
